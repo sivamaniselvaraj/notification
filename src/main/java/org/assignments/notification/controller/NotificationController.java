29027a144860fb66987b8c6553f61487f7450f25
sivamani selvaraj <talk2sivamani@gmail.com>
2026-04-04 22:07:41 +0530

    implemented open api

--- src/main/java/org/assignments/notification/controller/NotificationController.java
+++ src/main/java/org/assignments/notification/controller/NotificationController.java
@@ -1,5 +1,8 @@
 package org.assignments.notification.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.assignments.notification.dto.DeliveryStatusRequest;
 import org.assignments.notification.dto.DeliveryStatusResponse;
@@ -29,11 +32,37 @@ public class NotificationController {
     }
 
     @GetMapping("/status/{orderId}")
+    @Operation(summary = "Get notification status order by ID", description = "Fetch notification details using order ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Order found"),
+            @ApiResponse(responseCode = "404", description = "Order not found")
+    })
     public ResponseEntity<List<DeliveryStatusResponse>> getStatusByOrderId(
             @PathVariable Long orderId) {
-        log.info("getStatusByOrderId {}", orderId);
-        List<DeliveryStatusResponse> response = deliveryStatusService.getByOrderId(orderId);
 
-        return ResponseEntity.ok(response);
+        long startTime = System.currentTimeMillis();
+        try {
+            List<DeliveryStatusResponse> deliveryStatusResponse = deliveryStatusService.getByOrderId(orderId);
+
+            if (deliveryStatusResponse == null) {
+                log.warn("GET /status/{} - Delivery status not found", orderId);
+                return ResponseEntity.notFound().build();
+            }
+
+            long timeTaken = System.currentTimeMillis() - startTime;
+
+            log.warn("GET /status/{} - Success | TimeTaken={}ms | Status=FOUND",
+                    orderId, timeTaken);
+
+            return ResponseEntity.ok(deliveryStatusResponse);
+
+        } catch (Exception ex) {
+
+            long timeTaken = System.currentTimeMillis() - startTime;
+
+            log.error("GET /status/{} - Failed | TimeTaken={}ms | Error={}",
+                    orderId, timeTaken, ex.getMessage(), ex);
+            throw ex;
+        }
     }
 }
